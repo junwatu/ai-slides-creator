0369e7d4cc73788e0ede819ee5b2405c7bbade09
Equan P. <equan.p@gmail.com>
2024-06-23 10:37:02 +0700

    use vite app url from server and client

---
+++ apps/server.js
@@ -0,0 +1,21 @@
+import path from 'path'
+import { URL } from 'url'
+import express from 'express'
+
+const app = express()
+// eslint-disable-next-line no-undef
+const apiURL = new URL(process.env.VITE_APP_URL)
+const hostname = apiURL.hostname
+const port = apiURL.port || 4000
+
+app.use(express.json())
+app.use(express.static(path.join(path.resolve(), 'dist')))
+
+app.get('/', (req, res) => {
+	// Serve the index.html file from the 'dist' folder
+	res.sendFile(path.join(path.resolve(), 'dist', 'index.html'))
+})
+
+app.listen(port, hostname, () => {
+	console.log(`Server is running at http://${hostname}:${port}`)
+})
